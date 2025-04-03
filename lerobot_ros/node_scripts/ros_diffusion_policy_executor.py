4e0b411dcd4f41bc736749178c20faf6eb3aed6a
Kanazawanaoaki <naoaki65k@gmail.com>
2025-04-03 15:24:53 +0900

    write ros diffusion policy executor tmp

--- lerobot_ros/node_scripts/ros_diffusion_policy_executor.py
+++ lerobot_ros/node_scripts/ros_diffusion_policy_executor.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 import argparse
 import cv2
 import glob
@@ -126,77 +129,32 @@ class ObsManager(object):
             print(action_np)
 
 class InferenceNode(object):
-    def __init__(self, cfg: Dict[str, Any]):
+    def __init__(self, cfg: Dict[str, Any], n_pixel=112):
          # ノードの初期化
-        rospy.init_node('inference_node', anonymous=True)
+        rospy.init_node('diffusion_policy_executor', anonymous=True)
 
-        import ipdb
-        ipdb.set_trace()
+        # import ipdb
+        # ipdb.set_trace()
+
+        self.current_head_image = None
+        self.current_second_image = None
+        self.current_robot_state = None
 
         self.head_image_sub = rospy.Subscriber(cfg.obs.head_image.topic_name, CompressedImage, self.head_image_callback)
         self.second_image_sub = rospy.Subscriber(cfg.obs.second_image.topic_name, CompressedImage, self.second_image_callback)
+        self.robot_state_sub = rospy.Subscriber(cfg.obs.robot_state.topic_name, FloatVector, self.robot_state_callback)
 
-        ### TODO from here
-        self.robot_state_sub = rospy.Subscriber(cfg.obs.second_image.topic_name, CompressedImage, self.second_image_callback)
+        self.robot_action_pub = rospy.Publisher(cfg.actions.topic_name, FloatVector, queue_size=10)
 
-        self.robot_action_pub = rospy.Publisher('/image_size', String, queue_size=10)
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.timer_callback)
 
         # CvBridgeインスタンスの作成
         self.bridge = CvBridge()
 
-    def image_callback(self, msg):
-        try:
-            # ROSメッセージからOpenCV画像に変換
-            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        self.n_pixel = n_pixel
+        self.config = cfg
 
-            # 画像のサイズを取得
-            height, width, _ = cv_image.shape
-            image_size = f"Width: {width}, Height: {height}"
-
-            # サイズ情報をStringメッセージとしてパブリッシュ
-            self.string_pub.publish(image_size)
-
-        except Exception as e:
-            rospy.logerr(f"Error converting image: {e}")
-
-    def spin(self):
-        # ノードを実行し続ける
-        rospy.spin()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # parser.add_argument("--feedback", action="store_tr", help="feedback mode")
-    parser.add_argument("-pn", type=str, default="wrapping", help="project name")
-    parser.add_argument("--obs_txt_file", default="sub_obs/obs.txt")
-    parser.add_argument("-pp", type=str, help="project path name.")
-    parser.add_argument("-n", type=int, default=100, help="epoch num")
-    parser.add_argument("-m", type=int, default=112, help="pixel num")
-    parser.add_argument("-model", type=str, default="lstm", help="select prop model if --feedback specified")
-    parser.add_argument("-untouch", type=int, default=5, help="num of untouch episode")
-    parser.add_argument("--model_dir", type=str, default="outputs/train/wrapping")
-    args = parser.parse_args()
-    n_epoch: int = args.n
-    n_pixel: int = args.m
-    # feedback_mode: bool = args.feedback
-    project_name: str = args.pn
-    n_untouch: int = args.untouch
-    project_path_str: Optional[str] = args.pp
-    model_str: str = args.model
-    model_dir: str = args.model_dir
-
-    config = get_config_from_project_name(args.pn)
-    config.obs_txt_file = args.obs_txt_file
-
-    # remove txt file
-    with open("pub_act/pub_act.txt", 'w') as file:
-        pass
-
-    inference_node = InferenceNode(config)
-
-    feedback_mode = True
-    if feedback_mode:
-        om = ObsManager(config)
+        # setup policy
         with Path("./data/stats.pkl").open("rb") as f:
             stats = pickle.load(f)
         for key, value in stats.items():
@@ -204,7 +162,7 @@ if __name__ == "__main__":
                 stats[key][key_sub] = value_sub.to("cuda")
         print(stats)
 
-        resol = 112
+        resol = self.n_pixel
         camera_names = ["head", "second"]
         input_shapes = {"observation.state": [8]} # 14
         for name in camera_names:
@@ -226,7 +184,7 @@ if __name__ == "__main__":
         stats[key] = inner_dict
 
         policy = DiffusionPolicy(cfg, dataset_stats=stats)
-        policy = policy.to("cuda")
+        self.policy = policy.to("cuda")
         # print("#######################")
         # print(torch.load(os.path.join(model_dir, "model.pth")).keys())
         # print("###########################")
@@ -234,6 +192,107 @@ if __name__ == "__main__":
         # pretrained_weights = torch.load(os.path.join(model_dir, "model.pth"))
         pretrained_weights = load_file(os.path.join(model_dir, "model.safetensors"))
         # fixed_weights = rename_keys(pretrained_weights)
-        policy.load_state_dict(pretrained_weights)
+        self.policy.load_state_dict(pretrained_weights)
 
-        om.feedback(policy, n_pixel)
+    def head_image_callback(self, msg):
+        try:
+            # ROSメッセージからOpenCV画像に変換
+            # cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            # cv_image = self.bridge.compressed_imgmsg_to_cv2(msg, "rgb8").astype(np.uinit8)
+            np_arr = np.frombuffer(msg.data, np.uint8)
+            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            self.current_head_image = cv_image
+
+        except Exception as e:
+            rospy.logerr(f"Error converting image: {e}")
+
+    def second_image_callback(self, msg):
+        try:
+            # ROSメッセージからOpenCV画像に変換
+            # cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            # cv_image = self.bridge.compressed_imgmsg_to_cv2(msg, "rgb8").astype(np.uinit8)
+            np_arr = np.frombuffer(msg.data, np.uint8)
+            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            self.current_second_image = cv_image
+
+        except Exception as e:
+            rospy.logerr(f"Error converting image: {e}")
+
+    def robot_state_callback(self, msg):
+        try:
+            # ROSメッセージからOpenCV画像に変換
+            # cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            robot_state = msg.data
+
+            self.current_robot_state = robot_state
+
+        except Exception as e:
+            rospy.logerr(f"Error converting robot_state: {e}")
+
+    def timer_callback(self, event):
+        if (self.current_head_image is not  None) and (self.current_second_image is not  None) (self.current_robot_state is not  None):
+            head_rgb = cv2.resize(self.current_head_image, tuple(self.config['obs']['head_image'].dim[:2]), interpolation=cv2.INTER_LANCZOS4)
+            second_rgb = cv2.resize(self.current_second_image, tuple(self.config['obs']['head_image'].dim[:2]), interpolation=cv2.INTER_LANCZOS4)
+            head_image = torch.from_numpy(head_rgb).float()
+            second_image = torch.from_numpy(second_rgb).float()
+            head_image = head_image.to(torch.float32) / 255
+            second_image = second_image.to(torch.float32) / 255
+            head_image = head_image.permute(2, 0, 1)
+            second_image = second_image.permute(2, 0, 1)
+            head_image = head_image.unsqueeze(0)
+            second_image = second_image.unsqueeze(0)
+
+            state = torch.from_numpy(np.array(self.current_robot_state)).float().unsqueeze(0)
+            # state = torch.from_numpy(np.array(data['float_list']).float().unsqueeze(0)
+            head_image = head_image.to("cuda")
+            second_image = second_image.to("cuda")
+            state = state.to("cuda")
+
+            observation = {
+                # "observation.image": head_image,
+                "observation.image.head": head_image,
+                "observation.image.second": second_image,
+                "observation.state": state
+            }
+            with torch.inference_mode():
+                action = self.policy.select_action(observation)
+            action_np = action.detach().cpu().numpy().flatten()
+            print(action_np)
+
+            action_msg = FloatVector()
+            action_msg.header.stamp = rospy.Time.now()
+            action_msg.data = action_np
+            self.robot_action_pub.publish()
+
+
+    def spin(self):
+        # ノードを実行し続ける
+        rospy.spin()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("--feedback", action="store_tr", help="feedback mode")
+    parser.add_argument("-pn", type=str, default="wrapping", help="project name")
+    # parser.add_argument("--obs_txt_file", default="sub_obs/obs.txt")
+    parser.add_argument("-pp", type=str, help="project path name.")
+    parser.add_argument("-n", type=int, default=100, help="epoch num")
+    parser.add_argument("-m", type=int, default=112, help="pixel num")
+    parser.add_argument("-model", type=str, default="lstm", help="select prop model if --feedback specified")
+    parser.add_argument("-untouch", type=int, default=5, help="num of untouch episode")
+    parser.add_argument("--model_dir", type=str, default="outputs/train/wrapping")
+    args = parser.parse_args()
+    n_epoch: int = args.n
+    n_pixel: int = args.m
+    # feedback_mode: bool = args.feedback
+    project_name: str = args.pn
+    n_untouch: int = args.untouch
+    project_path_str: Optional[str] = args.pp
+    model_str: str = args.model
+    model_dir: str = args.model_dir
+
+    config = get_config_from_project_name(args.pn)
+
+    inference_node = InferenceNode(config)
+    inference_node.spin()
